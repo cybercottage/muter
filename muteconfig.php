308ceeaa4f7a791394f77c328ccdb99c2586becd
Ian Plain <cybercottage@users.noreply.github.com>
2025-10-13 18:07:36 +0100

    Create muteconfig.php

---
+++ muteconfig.php
@@ -0,0 +1,7 @@
+<?php
+// ==== CONFIGURATION ==== //
+$ami_host = "127.0.0.1";   // Asterisk server IP
+$ami_port = 5038;           // Default AMI port
+$ami_user = "AMIuser";        // Your AMI username
+$ami_pass = "AMIpassword";     // Your AMI password
+?>
